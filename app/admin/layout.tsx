077eead6c6fba195f60a7bcac1beb9cdfdcfad40
Mohamed Abdillahi <mahamadamiin140@gmail.com>
2024-07-29 16:32:14 +0300

    style: stretch main section

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export default function AdminLayout({ children }: Props) {
   return (
-    <div className="container p-4 max-w-screen-xl min-h-dvh flex flex-col lg:grid lg:grid-cols-[200px_1fr] lg:items-start gap-4">
+    <div className="flex-1 container p-4 max-w-screen-xl flex flex-col lg:grid lg:grid-cols-[200px_1fr] lg:items-start gap-4">
       <Sidebar />
-      <main className="flex-1 flex flex-col bg-background rounded-lg">{children}</main>
+      <main className="self-stretch flex flex-col bg-background rounded-lg">{children}</main>
     </div>
   )
 }
