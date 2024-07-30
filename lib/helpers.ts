810688ca866c2ed0f03b110979f13a4a20952908
Mohamed Abdillahi <mahamadamiin140@gmail.com>
2024-07-30 22:04:58 +0300

    feat: added dataURLtoFile function

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -26,3 +26,17 @@ export function getFileExtensionFromBase64(base64: string): string | null {
   // Return the corresponding file extension or null if not found
   return mimeTypes[mimeType] || null
 }
+
+export function dataURLtoFile(dataurl: string, filename: string) {
+  const arr = dataurl.split(',')
+  const mime = arr[0].match(/:(.*?);/)?.[1]
+  const bstr = atob(arr[arr.length - 1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  
+  return new File([u8arr], filename, { type: mime })
+}
