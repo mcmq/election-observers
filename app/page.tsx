30eb4b32f122868f918c000bd74024f66a0e235c
Mohamed Abdillahi <mahamadamiin140@gmail.com>
2024-07-31 00:36:53 +0300

    style: changed layout

--- app/page.tsx
+++ app/page.tsx
@@ -25,49 +25,46 @@ export default async function UserProfile({}: Props) {
     .getPublicUrl(userData?.image || '')
 
   return (
-    <div className="p-4">
-      <section className="container flex flex-col items-center p-4 max-w-screen-lg rounded-lg bg-background">
-        <Avatar className="size-24">
-          <AvatarImage src={publicUrl || ''} />
-          <AvatarFallback>
-            <User2 className="size-10 text-muted-foreground" />
-          </AvatarFallback>
-        </Avatar>
-        <h3 className="text-xl font-semibold">{user?.user_metadata.name}</h3>
-        <p className="text-sm text-muted-foreground font-medium">{user?.email}</p>
-      </section>
-      <section className="container grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 max-w-screen-lg rounded-lg bg-background">
-        <div className="flex flex-col gap-1 border p-4 rounded-lg">
-          <p className="text-lg font-semibold">{userData?.phone}</p>
-          <h4 className="font-medium text-sm text-muted-foreground">Phone Number</h4>
-        </div>
-        <div className="flex flex-col gap-1 border p-4 rounded-lg">
-          <p className="text-lg font-semibold">{userData?.pob}</p>
-          <h4 className="font-medium text-sm text-muted-foreground">Place of Birth</h4>
-        </div>
-        <div className="flex flex-col gap-1 border p-4 rounded-lg">
-          <p className="text-lg font-semibold">{userData?.dob}</p>
-          <h4 className="font-medium text-sm text-muted-foreground">Date of Birth</h4>
-        </div>
-      </section>
-      <section className="container my-2 flex flex-col gap-2 md:grid md:grid-cols-2 items-center p-4 max-w-screen-lg rounded-lg bg-background">
+    <div className="container p-4 max-w-screen-lg grid grid-cols-[200px_1fr] gap-3">
+      <aside className="flex flex-col gap-2 p-4 rounded-lg bg-background">
         <Link
           href="/settings"
           className={cn(
             buttonVariants({
               variant: 'outline',
-              size: 'lg',
-              className: 'justify-between'
+              // size: 'lg',
+              className: 'gap-3 justify-start'
             })
           )}
         >
-          <div className="flex gap-3 items-center">
-            <Settings className="size-4" />
-            <span>Settings</span>
-          </div>
-          <ChevronRight className="size-4" />
+          <Settings className="size-4" />
+          <span>Settings</span>
         </Link>
         <UserSignOut />
+      </aside>
+      <section className="container grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 rounded-lg bg-background">
+        <div className="col-span-full place-self-center flex flex-col gap-1">
+          <Avatar className="size-24">
+            <AvatarImage src={publicUrl || ''} />
+            <AvatarFallback>
+              <User2 className="size-10 text-muted-foreground" />
+            </AvatarFallback>
+          </Avatar>
+          <h3 className="text-xl font-semibold">{user?.user_metadata.display_name}</h3>
+          <p className="text-sm text-muted-foreground font-medium">{user?.email}</p>
+        </div>
+        <div className="flex flex-col gap-1 border p-4 rounded-lg">
+          <p className="text-lg md:text-base font-semibold">{userData?.phone}</p>
+          <h4 className="font-medium text-sm text-muted-foreground">Phone Number</h4>
+        </div>
+        <div className="flex flex-col gap-1 border p-4 rounded-lg">
+          <p className="text-lg md:text-base font-semibold">{userData?.pob}</p>
+          <h4 className="font-medium text-sm text-muted-foreground">Place of Birth</h4>
+        </div>
+        <div className="flex flex-col gap-1 border p-4 rounded-lg">
+          <p className="text-lg md:text-base font-semibold">{userData?.dob}</p>
+          <h4 className="font-medium text-sm text-muted-foreground">Date of Birth</h4>
+        </div>
       </section>
     </div>
   )
